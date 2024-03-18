8985df9ebc8a13efba9f522fc3d53d1912df2d2c
Shoaib Khan <shoaiba.khan@mail.utoronto.ca>
2024-03-18 01:21:56 -0400

    CORS issue hopefully solved

--- next.config.js
+++ next.config.js
@@ -7,11 +7,10 @@ const nextConfig = {
           destination:
             process.env.NODE_ENV === "development"
               ? "http://127.0.0.1:8000/api/:path*"
-              : "/api/",
+              : "/api/:path*",
         }
       ];
     },
   };
-  
 
 module.exports = nextConfig;
