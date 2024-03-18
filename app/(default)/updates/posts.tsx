ec316904032142d1b6eceaf3917159e98473c2f2
Shoaib Khan <Shoaibthedev@gmail.com>
2024-03-18 01:09:33 -0400

    Updates, Contact, and footer content updated (#4)
    * Updates, Contact, and footer content updated
    
    * middleware file removed, was just used in testing
    
    * Spam model up and connected with front-end. Front end needs polishing still
    
    ---------
    
    Co-authored-by: Shoaib Khan <shoaiba.khan@mail.utoronto.ca>

--- app/(default)/updates/posts.tsx
+++ app/(default)/updates/posts.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-import PostsSidebar from "./posts-sidebar";
+// import PostsSidebar from "./posts-sidebar";
 import PostsList from "./posts-list";
 
 export default function Posts({ posts }: { posts: Post[] }) {
@@ -17,11 +17,11 @@ export default function Posts({ posts }: { posts: Post[] }) {
           <div className="max-w-5xl mx-auto">
             <div className="md:flex md:justify-between">
               {/* Sidebar */}
-              <PostsSidebar
+              {/* <PostsSidebar
                 categories={categories}
                 selectedCategory={selectedCategory}
                 onCategoryChange={setSelectedCategory}
-              />
+              /> */}
 
               {/* Posts */}
               <PostsList posts={posts} selectedCategory={selectedCategory} />
