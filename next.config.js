f6436ec4cbb6ef07d4d63eac5345f42c60157217
Shoaib Khan <shoaiba.khan@mail.utoronto.ca>
2024-03-18 13:55:12 -0400

    Fixed????

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => {
-      return [
-        {
-          source: "/api/:path*",
-          destination:
-            process.env.NODE_ENV === "development"
-              ? "http://127.0.0.1:8000/api/:path*"
-              : "/api/",
-        }
-      ];
-    },
-  };
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      }
+    ];
+  },
+  includeFiles: {
+    "app/index.py": true,
+    "requirements.txt": true,
+  },
+};
 
 module.exports = nextConfig;
